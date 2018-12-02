5dd9ca87200dc01791ac047df7f784400efae734
sarpong <abasimisarpong@rocketmail.com>
2018-12-02 21:12:11 +0000

    learning how to query database

--- home/models.py
+++ home/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Animals(models.Model):
-    first_name =models.CharField(max_length=64, default=None)
-    last_name=models.CharField(max_length=64, default=None)
-
+class House(models.Model):
+    name = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
 
     def __str__(self):
-        return f" firstname : {self.first_name} , last_name:{self.last_name}"
+        return f"{self.id}-{self.name}-{self.street}"
