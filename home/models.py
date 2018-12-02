33f6ac36627e2dbb7d0094f177788188a46a0ead
sarpong <abasimisarpong@rocketmail.com>
2018-12-02 19:51:59 +0000

    python shell,migration

--- home/models.py
+++ home/models.py
@@ -2,15 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Animals(models.Model):
-    def __init__(self,first_name,last_name):
-        self.first_name =models.charField(max_length=64)
-        self.last_name=models.charField(max_length=64)
+    first_name =models.CharField(max_length=64, default=None)
+    last_name=models.CharField(max_length=64, default=None)
 
-    def get_firstName(self):
-        return self.first_name
-
-    def get_lastName(self):
-        return self.last_name
 
     def __str__(self):
-        return f" firstname : {self.first_name} , last_name:{self.last_name} "
+        return f" firstname : {self.first_name} , last_name:{self.last_name}"
