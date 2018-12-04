943b3e92949cef5e4d43b072111e12ae1683a157
sarpong <abasimisarpong@rocketmail.com>
2018-12-04 17:35:23 +0000

    adding expections to view pages if user being query for that not exit

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns=[
     path('',views.Home,name='home-page'),
-    path('about/',views.About,name='about-page')
+    path('about/',views.About,name='about-page'),
+    path('<int:userid>/userid/',views.GetUser,name='user-id')
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import Http404
 
 # Create your views here.
 def Home(request):
@@ -8,3 +10,15 @@ def Home(request):
 def About(request):
     template='home/about.html'
     return render(request,template)
+
+#Raise an expection when user being serch does not exit
+def GetUser(request,userid):
+    template='home/userid.html'
+    try:
+        getUser=User.objects.get(id=userid)
+    except User.DoesNotExist:
+        raise Http404("Sorry the user with that Id does not exist") 
+    context={
+        'getUser':getUser
+    }
+    return render(request,template,context)
