61566d1be2404801e976bf5cd2acb7007e92e25e
sarpong <abasimisarpong@rocketmail.com>
2018-12-04 02:15:19 +0000

    I created the post model

--- home/models.py
+++ home/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
+class Posts(models.Model):
+    class Meta:
+        verbose_name_plural='Posts'
 
+    textArea=models.TextField(max_length=240)
+    date_posted=models.DateTimeField(default=timezone.now)
+    authour=models.ForeignKey(User,on_delete=models.CASCADE)
