94bbb68cb8c6cb213e914239e3f38bddc8a17c4c
sarpong <abasimisarpong@rocketmail.com>
2018-12-07 19:36:18 +0000

    Building the user registration and auth page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def userRegistrationForm(request):
+    form=UserCreationForm()
+    template = 'users/register.html'
+    context ={
+        'form':form
+    }
+    return render(request,template,context)
