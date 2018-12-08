e87c1f57d07ab83820e833c5d583047586f5736b
sarpong <abasimisarpong@rocketmail.com>
2018-12-08 23:23:51 +0000

    I have added a user login page,so currently user login and registration working

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from .import views
 
 
 urlpatterns= [
-    path('register/',views.userRegistrationForm,name='user_registration'),
-    path('login/',views.login,name='login-page')
+    path('register/',views.userRegistrationForm,name='user_registration')
 ]
