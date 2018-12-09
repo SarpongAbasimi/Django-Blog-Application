7069f1ce8b248c8fcabdd1128633403b3001d0ac
sarpong <abasimisarpong@rocketmail.com>
2018-12-09 03:23:26 +0000

    I added a login link to the base nav

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     path('',include('users.urls')),
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'))
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login-page')
    # path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'))
 ]
